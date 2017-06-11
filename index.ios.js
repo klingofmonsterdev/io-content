f9c4c0bb53a10c673c61c3a154275149c65ac6a7
Charin <klingofmonsterdev@gmail.com>
2017-06-12 00:51:03 +0700

    ส่งการบ้านครั้งที่1

---
+++ index.ios.js
@@ -0,0 +1,44 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import Login from './src/login/Login';
+
+export default class react_v1 extends Component {
+  render() {
+    return (
+      <Login />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('react_v1', () => react_v1);
